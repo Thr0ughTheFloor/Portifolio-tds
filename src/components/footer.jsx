8afc91429108a5b8626b3d1a49dc57767439e5e2
Fabio Luis Batista Santos <contatofabio2005@gmail.com>
2025-04-14 11:34:23 -0300

    Add files via upload

---
+++ src/components/footer.jsx
@@ -0,0 +1,23 @@
+import Style from './footer.module.css'
+import facebook from '../assets/images/facebook.png'
+import instagram from '../assets/images/instagram.png'
+
+function MeuFooter(){
+    return(
+        <>
+        <footer>
+            <section className={Style.footerSection}>
+            <div className={Style.Container_ig}>
+                <a href="https://www.instagram.com/changingwalls/"><img src={instagram} alt="instagram-logo" /></a>
+            </div>
+            <div className={Style.Container_fb}>
+                <a href="https://www.facebook.com/305man/"><img src={facebook} alt="instagram-logo" /></a>
+            </div>
+ 
+            </section>
+        </footer>
+        </>
+    )
+}
+
+export default MeuFooter
